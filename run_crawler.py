304ed518fbfb3353514adc3c257ce3821391bcc9
nladuo <kalen25115@gmail.com>
2017-05-02 15:48:01 +0800

    modified:   run_crawler.py

--- run_crawler.py
+++ run_crawler.py
@@ -1,14 +1,19 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """ 执行爬虫, 并对对房源进行信息统计 """
-import scrapy
-import os
-from lianjia_crawler.pipelines import MongoDBPipeline
+import scrapydo
 import time
 import sys
+import logging
+from lianjia_crawler.pipelines import MongoDBPipeline
+from lianjia_crawler.spiders.item_spider import ItemSpider
+from lianjia_crawler.spiders.link_spider import LinkSpider
+
 
 reload(sys)
 sys.setdefaultencoding('utf8')
+
+scrapydo.setup()
 
 
 def summarize():
@@ -16,25 +21,32 @@ def summarize():
 
 
 if __name__ == "__main__":
+
+    logging.basicConfig(
+        filename='spider.log',
+        format='%(levelname)s %(asctime)s: %(message)s',
+        level=logging.DEBUG
+    )
+
     mongo = MongoDBPipeline()
     t = time.time()
 
     # 判断是否爬取了link
     if mongo.get_links().count() == 0:
         print "爬取地区链接...."
-        os.system("scrapy crawl link")
+        scrapydo.run_spider(LinkSpider)
 
     # 爬取item
     while True:
         print "爬取房源中....."
-        os.system("scrapy crawl item")
+        scrapydo.run_spider(ItemSpider)
         if mongo.get_failed_urls().count() == 0:
             break
         print "休息一天再爬...."
         time.sleep(24 * 3600)  # 一天之后再爬
 
     # 进行统计
-    print "爬取结束, 耗时%d" % (time.time() - t)
+    print "爬取结束, 耗时%d秒" % (time.time() - t)
     summarize()
 
     # 清空links表
