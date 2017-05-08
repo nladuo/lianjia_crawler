095be7f372092bea7565f399a985b23665b05cd6
nladuo <kalen25115@gmail.com>
2017-05-08 15:04:51 +0800

    updated crawler.

--- lianjia_crawler/spiders/link_spider.py
+++ lianjia_crawler/spiders/link_spider.py
@@ -12,34 +12,32 @@ class LinkSpider(scrapy.Spider):
     ]
 
     def parse(self, response):
-        try:
-            for detail in response.css("div.position dd div")[0].css("a"):
-                url = "http://bj.lianjia.com/%s" % detail.css('a::attr(href)').\
-                    extract_first()
-                print url
-                district = detail.css('a::text').extract_first()
-                yield scrapy.Request(url, callback=lambda r, k=url, i=district: self.parse_detail(r, k, i))
-        except:
-            # 出错重新爬
+        if response.status != 200 or not response.url.startswith("http://bj.lianjia"):
             yield scrapy.Request(self.start_urls[0], callback=self.parse)
 
-    def parse_detail(self, response, url, district):
-        try:
-            district_item = DistrictItem()
-            district_item["url"] = response.url
-            district_item["name"] = district
-            locations = []
-            for detail in response.css("div.position dd div div")[1].css("a"):
-                link = LinkItem()
-                link["district"] = district
-                link["location"] = detail.css('a::text').extract_first()
-                link["url"] = "http://bj.lianjia.com/%s" % detail.css('a::attr(href)').\
-                    extract_first()
-                locations.append(link["location"])
-                print link["url"]
-                yield link
-            district_item["locations"] = json.dumps(locations)
-            yield district_item
-        except:
-            # 出错重新爬
+        for detail in response.css("div.position dd div")[0].css("a"):
+            url = "http://bj.lianjia.com/%s" % detail.css('a::attr(href)').\
+                extract_first()
+            print url
+            district = detail.css('a::text').extract_first()
             yield scrapy.Request(url, callback=lambda r, k=url, i=district: self.parse_detail(r, k, i))
+
+    def parse_detail(self, response, url, district):
+        if response.status != 200 or not response.url.startswith("http://bj.lianjia"):
+            yield scrapy.Request(url, callback=lambda r, k=url, i=district: self.parse_detail(r, k, i))
+
+        district_item = DistrictItem()
+        district_item["url"] = response.url
+        district_item["name"] = district
+        locations = []
+        for detail in response.css("div.position dd div div")[1].css("a"):
+            link = LinkItem()
+            link["district"] = district
+            link["location"] = detail.css('a::text').extract_first()
+            link["url"] = "http://bj.lianjia.com/%s" % detail.css('a::attr(href)').\
+                extract_first()
+            locations.append(link["location"])
+            print link["url"]
+            yield link
+        district_item["locations"] = json.dumps(locations)
+        yield district_item
